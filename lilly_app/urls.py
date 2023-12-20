d83fa9b2c9d5909989aac9ac1e62a6f762e73861
Saromazzotta <Saromazzotta@gmail.com>
2023-12-19 19:31:25 -0500

    added edit template and url paths

--- lilly_app/urls.py
+++ lilly_app/urls.py
@@ -5,5 +5,10 @@ urlpatterns = [
     path('welcome/', views.first_view),
     path('create/', views.create_coupon, name='create_coupon'),
     path('created/', views.post_coupon, name='post_coupon'),
-    path('view/<int:coupon_id>/', views.view_coupon, name='view_coupon')
+    path('view/<int:coupon_id>/', views.view_coupon, name='view_coupon'),
+    path('edit/<int:coupon_id>/', views.edit_coupon, name='edit_coupon'),
+    path('update/<int:coupon_id/', views.update_coupon, name='update_coupon'),
+    path('delete/<int:coupon_id>/', views.delete_coupon, name='delete_coupon')
+
+
 ]
