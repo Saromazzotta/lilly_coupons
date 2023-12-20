92937ce01d7003b3a4d827221ac5ec9d1744de92
Saromazzotta <Saromazzotta@gmail.com>
2023-12-19 19:27:06 -0500

    made first migration, added CRD views

--- lilly_app/urls.py
+++ lilly_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('welcome/', views.first_view),
     path('create/', views.create_coupon, name='create_coupon'),
-    path('created/', views.post_coupon, name='post_coupon')
+    path('created/', views.post_coupon, name='post_coupon'),
+    path('view/<int:coupon_id>/', views.view_coupon, name='view_coupon')
 ]
--- lilly_app/views.py
+++ lilly_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Coupon
 
 def first_view(request):
     return HttpResponse('Hello Lilly!')
@@ -18,5 +19,28 @@ def post_coupon(request):
             'date': request.POST.get('date'),
         }
         request.session['coupon_data'] = coupon_data # Store the coupon data in session
+        #added in DB
+        new_coupon = Coupon.objects.create(**coupon_data) # Create a new coupon instance with the POST data
         return redirect('dashboard') # Redirect to the 'dashboard' view
-    return render(request, 'dashboard')
+    return render(request, 'dashboard') # If not a POST request, render the dashboard template
+
+def view_coupon(request, coupon_id):
+    # print(request.session.items())
+    coupon = Coupon.objects.get(id=coupon_id) # Retrieve the Coupon instance with the given coupon_id
+    context = {
+        "coupon": coupon
+    }
+    return render(request, 'view_coupon.html', context) # Render the view_character.html template with the context
+
+def edit_coupon(request, coupon_id):
+    # print(request.session.items())
+    coupon = Coupon.objects.get(id=coupon_id) # Retrieve the Coupon instance with the given coupon_id
+    context = {
+        "coupon": coupon
+    }
+    return render(request, 'edit_coupon.html', context) # Render the edit_character.html template with the context
+
+def delete_coupon(request, coupon_id):
+    coupon = Coupon.objects.get(id=coupon_id) # Retrieve the Coupon instance with the given coupon_id
+    coupon.delete # Delete the retrieved character
+    return redirect(request) # Redirect to the dashboard view
